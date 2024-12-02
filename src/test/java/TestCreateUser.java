1a81c06182a7e07ff5d875a4ec0f14facd87f9b6
Rodion Shumilov <rodya7911@gmal.com>
2024-12-02 23:20:10 +0300

    something 1 class

--- src/test/java/TestCreateUser.java
+++ src/test/java/TestCreateUser.java
@@ -1,34 +1,76 @@
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class TestCreateUser {
-    private String bearerToken;
-    CreateUserData createUserData = new CreateUserData("bogomol3000@yandex.ru", "1234567890", "Bogdan");
-    // оздаём уникального пользователя
-    @Test
-    public void createUniqueUser(){
-        Response response = given()
-                //.header("Content-type", "application/json")
-                .and()
-                .body(createUserData)
-                .when()
-                .post("/api/auth/register")
-                .then().statusCode(201)
-                .extract()
-                .response();
-        bearerToken = response.jsonPath().getString("accessToken").replace("Bearer ", "");
+    // Переменные класса
+    private String token;
+    // Прописываем Before с главной ссылкой
+    @Before
+    public void setUp(){
+        baseURI = "https://stellarburgers.nomoreparties.site";
     }
-    @After
-    public void tearDown(){
-        given()
-                .headers("Content-type", "application/json", "Authorization", "Bearer" + bearerToken)
+    @Test
+    public void testRegisterUserSuccessfully() {
+
+        // Данные пользователя
+        User user = new User("exampler.praktikum@gmail.com", "praktikum", "praktikum");
+
+        // Отправляем запрос на регистрацию
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .body(user)
                 .when()
-                .delete()
-                .then()
-                .statusCode(202);
+                .post("/api/auth/register");
+
+        // Проверяем статус-код и тело ответа
+        response.then()
+                .statusCode(200) // Регистрация должна вернуть 200
+                .body("success", equalTo(true));
+
+        // Сохраняем токен для удаления пользователя
+        token = response.jsonPath().getString("accessToken");
+    }
+    @Test
+    public void testRegisterUserRepeat(){
+        // Данные пользователя
+        User user = new User("examplerrr.praktikum@gmail.com", "praktikum", "praktikum");
+        // Отправляем запрос на регистрацию
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .body(user)
+                .when()
+                .post("/api/auth/register");
+                // Проверяем статус-код
+                response.then().statusCode(200);
+
+        //
+        Response response1 = given()
+                .header("Content-Type", "application/json")
+                .body(user)
+                .when()
+                .post("/api/auth/register");
+        // Проверяем статус-код
+        response1.then().statusCode(403);
+        // Сохраняем токен для удаления пользователя
+        token = response.jsonPath().getString("accessToken");
+    }
+
+
+    @After
+    public void tearDown() {
+        if (token != null) {
+            // Удаляем пользователя после теста
+            given()
+                    .header("Authorization", token)
+                    .when()
+                    .delete("/api/auth/user")
+                    .then()
+                    .statusCode(202);
+        }
     }
 }
